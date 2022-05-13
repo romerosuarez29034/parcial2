21074cb3bc36e92e2364cfc4f6333ef24aed242c
EMERSON <EMERSON@DESKTOP-OISSV4T>
2022-05-13 12:35:08 -0500

    Agregar archivos de proyecto.

---
+++ parcial2/abstraccion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2
+{
+    public abstract class Producto
+    {
+        protected double precioVenta;
+        protected double costoFabrica;
+        protected string nombreProducto;
+
+        public Producto(string nombre, double precio, double costo)
+        {
+            nombreProducto = nombre;
+            costoFabrica = costo;
+            precioVenta = precio;
+        }
+
+        public abstract string imprimirDatos();
+    }
+
+    public class Libro : Producto
+    {
+        public Libro(string titulo, double precio, double costo)
+            : base(titulo, precio, costo)
+        {
+
+        }
+
+        public override string imprimirDatos()
+        {
+            return "Libro: " + nombreProducto + ", Precio: " + precioVenta;
+        }
+    }
+
+    public class DVD : Producto
+    {
+        public DVD(string titulo, double precio, double costo)
+            : base(titulo, precio, costo)
+        {
+
+        }
+        public override string imprimirDatos()
+        {
+            return "DVD: " + nombreProducto + " Precio: " + precioVenta;
+        }
+
+    }
+}
+
+
+
+
---
+++ parcial2/polimorfismo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcial2
+{
+    public abstract class Persona
+    {
+        public string Nombre { get; set; }
+
+        public abstract string Saludar();
+    }
+    public class Alumno : Persona
+    {
+        public Alumno(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+        public override string Saludar()
+        {
+            string saludo = "Hola, mi nombre es " + this.Nombre + " y soy un alumno";
+            return saludo;
+        }
+    }
+    public class Empleado : Persona
+    {
+        public Empleado(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+        public override string Saludar()
+        {
+            string saludo = "Hola, mi nombre es " + this.Nombre + " y soy un empleado";
+            return saludo;
+        }
+    }
+}
+ 
